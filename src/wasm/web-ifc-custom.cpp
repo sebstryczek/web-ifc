ff68ce1cb0fde7620a677a589d8793ff35ba30fb
Sebastian <sebastian@stryczek.pl>
2023-09-18 17:56:44 +0200

    Add custom test file

--- src/wasm/web-ifc-custom.cpp
+++ src/wasm/web-ifc-custom.cpp
@@ -37,7 +37,8 @@ void print(std::string message)
 
 int main()
 {
-    std::string ifc_file_path = "/Users/sebastian/Desktop/InfraByte/__data/ifc/AC-20-Smiley-West-10-Bldg.ifc";
+    std::string ifc_file_path = "/Users/sebastian/Desktop/ifc-playground/_input/AC-20-Smiley-West-10-Bldg.ifc";
+    // std::string ifc_file_path = "/Users/sebastian/Desktop/ifc-playground/_input/wall.ifc";
     std::string content = ReadFile(ifc_file_path);
 
     webifc::utility::LoaderSettings set;
@@ -77,11 +78,119 @@ int main()
         {
             auto elementId = elements[i];
 
+            // auto mesh = geometryLoader.GetFlatMesh(elements[i]);
+
             auto mesh = geometryLoader.GetFlatMesh(elements[i]);
+
+            for (auto &geom : mesh.geometries)
+            {
+                auto flatGeom = geometryLoader.GetGeometry(geom.geometryExpressID);
+                // print("Flat geometry: " + std::to_string(flatGeom.GetVertexData()) + " " + std::to_string(flatGeom.GetVertexDataSize()));
+
+                // print(std::to_string(flatGeom.vertexData.at(0)));
+                // print(std::to_string(flatGeom.vertexData.at(1)));
+                // print(std::to_string(flatGeom.vertexData.at(2)));
+                // ifcapi.GetVertexArray(geometry.GetVertexData(), geometry.GetVertexDataSize());
+                //  this.getSubArray(this.wasmModule.HEAPF32, ptr, size);
+                // heap.subarray(startPtr / 4, startPtr / 4 + sizeBytes).slice(0);
+            }
         }
         sum_of_all_elements += elements.size();
     }
     print("Total number of elements: " + std::to_string(sum_of_all_elements));
+
+    // auto typeName = schema.IfcTypeCodeToType(2044713172);
+    // auto line = loader.GetLine(expressID);
+    // loader.MoveToArgumentOffset(line, 0);
+
+    size_t size = 0;
+    bool endOfLine = false;
+    auto expressID = loader.GetExpressIDsWithType(3512223829).at(0);
+    auto &line = loader.GetLine(expressID);
+    loader.MoveToArgumentOffset(line, 0);
+    while (!loader.IsAtEnd() && !endOfLine)
+    {
+        webifc::parsing::IfcTokenType t = loader.GetTokenType();
+
+        print("YO?" + std::to_string((int)t));
+        switch (t)
+        {
+        case webifc::parsing::IfcTokenType::LINE_END:
+        {
+            print("LINE_END");
+            endOfLine = true;
+            break;
+        }
+        case webifc::parsing::IfcTokenType::EMPTY:
+        {
+            print("EMPTY");
+            // arguments.set(size++, emscripten::val::null());
+            break;
+        }
+        case webifc::parsing::IfcTokenType::SET_BEGIN:
+        {
+            print("SET_BEGIN");
+            // arguments.set(size++, GetArgs(modelID));
+            break;
+        }
+        case webifc::parsing::IfcTokenType::SET_END:
+        {
+            print("SET_END");
+            endOfLine = true;
+            break;
+        }
+        case webifc::parsing::IfcTokenType::LABEL:
+        {
+            print("LABEL");
+            // read label
+            // auto obj = emscripten::val::object();
+            // obj.set("type", emscripten::val(static_cast<uint32_t>(webifc::parsing::IfcTokenType::LABEL)));
+            loader.StepBack();
+            auto s = loader.GetStringArgument();
+            auto typeCode = schemaManager.IfcTypeToTypeCode(s);
+            // obj.set("typecode", emscripten::val(typeCode));
+            // // read set open
+            // loader.GetTokenType();
+            // obj.set("value", GetArgs(modelID, true));
+            // arguments.set(size++, obj);
+            break;
+        }
+        case webifc::parsing::IfcTokenType::STRING:
+        case webifc::parsing::IfcTokenType::ENUM:
+        {
+            print("STRING/ENUM");
+            loader.StepBack();
+            break;
+        }
+        case webifc::parsing::IfcTokenType::REAL:
+        {
+            print("REAL");
+            loader.StepBack();
+            double d = loader.GetDoubleArgument();
+            print(std::to_string(d));
+            break;
+        }
+        case webifc::parsing::IfcTokenType::REF:
+        {
+            print("REF");
+            loader.StepBack();
+            // emscripten::val obj;
+            // if (inObject)
+            //     obj = ReadValue(modelID, t);
+            // else
+            // {
+            //     obj = emscripten::val::object();
+            //     obj.set("type", emscripten::val(static_cast<uint32_t>(t)));
+            //     obj.set("value", ReadValue(modelID, t));
+            // }
+            // arguments.set(size++, obj);
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
     // loader.
     // auto all = loader.GetExpressIDsWithType(webifc::schema::IFCROOT);
     // print(std::to_string(all.size()) + " objects loaded");
